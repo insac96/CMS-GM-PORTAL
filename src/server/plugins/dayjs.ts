df172485fac9476402cdccb6f3e1a3bce7ca20d5
Your Name <you@example.com>
2024-10-07 15:53:22 +0700

    Update

--- src/server/plugins/dayjs.ts
+++ src/server/plugins/dayjs.ts
@@ -1,11 +1,11 @@
 import dayjs from 'dayjs'
-import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekOfYear from 'dayjs/plugin/weekOfYear.js'
 
 declare global {
   var DayJS : any
 }
 
-export default defineNitroPlugin(async (nitroApp) => {
+export default defineNitroPlugin(async () => {
   dayjs.extend(weekOfYear)
   global.DayJS = dayjs
 })
