aa23e53b6ebac872148a14da604bb13e1b4c9a14
Your Name <you@example.com>
2024-10-07 17:32:39 +0700

    Update

--- src/client/middleware/whitelist.ts
+++ src/client/middleware/whitelist.ts
@@ -9,6 +9,6 @@ export default defineNuxtRouteMiddleware(async () => {
     return navigateTo('/manage/@eni')
   }
   catch (e:any) {
-    return navigateTo('/')
+    return navigateTo('/manage')
   }
 })
