d81ea4b257a2523e2d37df78b53c8d8856364140
Your Name <you@example.com>
2024-10-21 10:35:27 +0700

    Update

--- src/client/components/data/user/avatar.vue
+++ src/client/components/data/user/avatar.vue
@@ -2,11 +2,19 @@
   <div>
     <UiFlex class="relative m-3">
       <UAvatar :src="user.avatar" :alt="user.username" class="rounded-full"></UAvatar>
-      <img :src="`/images/user/level/${user.level ? (user.level.number || 1) : 1}.webp`" class="absolute" style="transform: scale(2.7) translateY(1.1px) translateX(0.1px)"/>
+      <img :src="`/images/user/level/${imgLevel}.webp`" class="absolute" style="transform: scale(2.7) translateY(1.1px) translateX(0.1px)"/>
     </UiFlex>
   </div>
 </template>
 
 <script setup>
 const props = defineProps(['user'])
+
+const imgLevel = computed(() => {
+  if(!props.user) return 1
+  if(!props.user.level) return 1
+  if(!props.user.type) return props.user.level.number || 1
+  if(props.user.type == 100) return 17
+  return props.user.level.number || 1
+})
 </script>
