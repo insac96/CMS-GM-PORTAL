1c0ab50ad181298fcfc35161df653855ebe47f09
Your Name <you@example.com>
2024-10-25 14:24:53 +0700

    Update

--- src/client/components/data/user/avatar.vue
+++ src/client/components/data/user/avatar.vue
@@ -4,11 +4,17 @@
       <UAvatar :src="user.avatar" :alt="user.username" class="rounded-full" :size="size || 'xs'"></UAvatar>
       <img :src="`/images/user/level/${level}.webp`" class="absolute" style="transform: scale(2.7) translateY(1.1px) translateX(0.1px)"/>
     </UiFlex>
+
+    <UModal v-model="view" :ui="{width: 'sm:max-w-[400px]'}">
+      <DataUserBox :fetch-id="user._id" />
+    </UModal>
   </div>
 </template>
 
 <script setup>
 const props = defineProps(['user', 'size'])
+
+const view = ref(false)
 
 const level = computed(() => {
   if(!props.user) return 1
