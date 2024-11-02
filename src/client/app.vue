8d877bb50dc8a0a33904538c7b75539721bd789c
Your Name <you@example.com>
2024-11-02 14:33:28 +0700

    Update

--- src/client/app.vue
+++ src/client/app.vue
@@ -1,6 +1,6 @@
 <template>
   <UiAnimStar />
-  <SocketNotifyGlobal />
+  <SocketNotifyGlobal v-if="route.meta.layout != 'play'"/>
   <NuxtLayout class="relative">
     <NuxtLoadingIndicator :height="2" />
     <NuxtPage />
@@ -14,6 +14,7 @@ import colors from '#tailwind-config/theme/colors'
 const { img } = useMakeLink()
 const appConfig = useAppConfig()
 const configStore = useConfigStore()
+const route = useRoute()
 
 // Meta Seo
 useSeoMeta({
